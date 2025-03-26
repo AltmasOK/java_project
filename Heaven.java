3192d8c2f4913b52d99f8653135b53cb8ae95cc3
AltmasOK <altmash.m2024@gmail.com>
2025-03-26 15:25:45 +0530

    Initial commit

---
+++ Heaven.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+public class Heaven{
+
+    public static void main(String[] args){
+
+        System.out.println("Heaven World");
+        System.out.println("Thank You");
+        System.out.println(new Date());
+    }
+}
